8ef81816d401cb3c71275ead3f38f4e4767f47f9
f3m3 <john.mcwilliams@f3mmedia.com>
2013-04-05 20:00:31 +0100

    page 108

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -6,13 +6,13 @@ require 'spec_helper'
     
         it "should have the h1 'Sample App' " do
           visit '/static_pages/home'
-          page.should have_selector ('h1', text => 'Sample App')
+          page.should have_selector ('h1', :text => 'Sample App')
     end
 
        it "should have the title 'Home' " do
           visit '/static_pages/home'
           page.should have_selector ('title',
-                               text => "Ruby on Rails Tutorial Sample App | Home")
+                               :text => "Ruby on Rails Tutorial Sample App | Home")
 
     end
   end 
@@ -21,13 +21,13 @@ require 'spec_helper'
 
         it "should have the h1 'Help' " do
           visit '/static_pages/help'
-          page.should have_selector ('h1', text=> 'Help')
+          page.should have_selector ('h1', :text=> 'Help')
     end
 
        it "should have the title 'Help' " do
           visit '/static_pages/help'
           page.should have_selector ('title',
-                               text=> "Ruby on Rails Tutorial Sample App | Help")
+                               :text=> "Ruby on Rails Tutorial Sample App | Help")
 
     end
   end 
@@ -36,13 +36,13 @@ require 'spec_helper'
 
          it "should have the h1 'About Us' " do
           visit '/static_pages/about'
-          page.should have_selector ('h1', text=> 'About Us')
+          page.should have_selector ('h1', :text=> 'About Us')
     end
 
        it "should have the title 'About Us' " do
           visit '/static_pages/about'
           page.should have_selector ('title',
-                               text=> "Ruby on Rails Tutorial Sample App | About Us")
+                               :text=> "Ruby on Rails Tutorial Sample App | About Us")
 
     end
   end 
