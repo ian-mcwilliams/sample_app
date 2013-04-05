92ba110420bfdc4391e9c23381854866609674e9
f3m3 <john.mcwilliams@f3mmedia.com>
2013-04-05 21:28:38 +0100

    page 108

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -8,8 +8,8 @@ require 'spec_helper'
           visit '/static_pages/home'
           page.should have_selector ('h1', text: => 'Sample App')
     end
-
-       it "should have the title 'Home' " do
+       it "should 
+have the title 'Home' " do
           visit '/static_pages/home'
           page.should have_selector ('title',
                                text: => "Ruby on Rails Tutorial Sample App | Home")
