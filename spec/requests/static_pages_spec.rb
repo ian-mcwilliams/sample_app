5a4e0b72376bd43c47d2ac6c10d6a2f24689e5c0
f3m3 <john.mcwilliams@f3mmedia.com>
2013-04-05 22:56:42 +0100

    have_content not_selector

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -10,8 +10,7 @@ require 'spec_helper'
     end
        it "should have the title 'Ruby on Rails Tutorial Sample App | Home' " do
           visit '/static_pages/home'
-          page.should have_content (
-                               'Ruby on Rails Tutorial Sample App | Home')
+          page.should have_content ('Ruby on Rails Tutorial Sample App | Home')
 
     end
   end 
@@ -25,8 +24,7 @@ require 'spec_helper'
 
        it "should have the title 'Ruby on Rails Tutorial Sample App | Help' " do
           visit '/static_pages/help'
-          page.should have_content (
-                               'Ruby on Rails Tutorial Sample App | Help')
+          page.should have_content ('Ruby on Rails Tutorial Sample App | Help')
 
     end
   end 
@@ -40,8 +38,7 @@ require 'spec_helper'
 
        it "should have the title 'Ruby on Rails Tutorial Sample App | About Us' " do
           visit '/static_pages/about'
-          page.should have_content (
-                               'Ruby on Rails Tutorial Sample App | About Us')
+          page.should have_content ('Ruby on Rails Tutorial Sample App | About Us')
 
     end
   end 
