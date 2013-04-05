ad03eb0e265cc8bcf02e6ece348b9bc56fc4c91a
f3m3 <john.mcwilliams@f3mmedia.com>
2013-04-05 22:28:33 +0100

    have_content not_selector

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -8,8 +8,7 @@ require 'spec_helper'
           visit '/static_pages/home'
           page.should have_content ('Sample App')
     end
-       it "should 
-have the title 'Home' " do
+       it "should have the title 'Ruby on Rails Tutorial Sample App | Home' " do
           visit '/static_pages/home'
           page.should have_content (
                                'Ruby on Rails Tutorial Sample App | Home')
@@ -24,7 +23,7 @@ have the title 'Home' " do
           page.should have_content ('Help')
     end
 
-       it "should have the title 'Help' " do
+       it "should have the title 'Ruby on Rails Tutorial Sample App | Help' " do
           visit '/static_pages/help'
           page.should have_content (
                                'Ruby on Rails Tutorial Sample App | Help')
@@ -36,7 +35,7 @@ have the title 'Home' " do
 
          it "should have the h1 'About Us' " do
           visit '/static_pages/about'
-          page.should have_content ('About Us')
+          page.should have_content ('Ruby on Rails Tutorial Sample App | About Us')
     end
 
        it "should have the title 'About Us' " do
