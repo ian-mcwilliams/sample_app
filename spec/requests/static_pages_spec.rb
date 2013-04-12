88ce1e2c7cc36153f25796d01c1a4099ebc18de3
F3M3 <john.mcwilliams@f3mmedia.com>
2013-04-12 18:10:35 +0100

    x

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -7,11 +7,13 @@ require 'spec_helper'
         it "should have the h1 'Sample App'" do
           visit '/static_pages/home'
           page.should have_selector ('h1', :text => 'Sample App')
+                            
     end
         it "should have the title 'Home'" do
           visit '/static_pages/home'
           page.should have_selector ('title',
-                           :text =>"Ruby on Rails Tutorial Sample App | Home")
+                            :text => "Ruby on Rails Tutorial Sample App | Home")
+                           
 
     end
   end 
@@ -22,6 +24,7 @@ require 'spec_helper'
           visit '/static_pages/help'
           page.should have_selector ('h1', :text => 'Help')
     end
+                            
 
          it "should have the title 'Help'" do
           visit '/static_pages/help'
@@ -36,6 +39,7 @@ require 'spec_helper'
          it "should have the h1 'About Us'" do
           visit '/static_pages/about'
           page.should have_selector ('h1', :text => 'About Us')
+
     end
 
          it "should have the title 'About Us' " do
@@ -46,12 +50,13 @@ require 'spec_helper'
 
         describe "Contact page" do
 
-         it "should have the h1 'About Us'" do
+         it "should have the h1 'Contact'" do
           visit '/static_pages/about'
           page.should have_selector ('h1', :text => 'Contact')
+                           
     end
 
-         it "should have the title 'About Us' " do
+         it "should have the title 'Contact' " do
           visit '/static_pages/about'
          page.should have_selector ('title',
                            :text => "Ruby on Rails Tutorial Sample App | Contact")
