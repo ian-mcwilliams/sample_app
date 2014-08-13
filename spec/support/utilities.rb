250a2fe8a9b95e9c731816ad77d74798df4bdd9b
ian-mcwilliams <ian.mcwilliams@f3mmedia.com>
2014-08-13 18:09:47 +0100

    Change form_for to form_tag for sign in; extra decoupling of tests from implementation in spec

--- spec/support/utilities.rb
+++ spec/support/utilities.rb
@@ -6,8 +6,31 @@ def valid_signin(user)
   click_button "Sign in"
 end
 
+def complete_valid_signup_form
+  user = test_user
+  fill_in "Name",         with: user[:name]
+  fill_in "Email",        with: user[:email]
+  fill_in "Password",     with: user[:password]
+  fill_in "Confirmation", with: user[:confirmation]
+end
+
+def test_user
+  {
+      name:         'Example User',
+      email:        'user@example.com',
+      password:     'foobar',
+      confirmation: 'foobar'
+  }
+end
+
 RSpec::Matchers.define :have_error_message do |message|
   match do |page|
     expect(page).to have_selector('div.alert.alert-error', text: message)
+  end
+end
+
+RSpec::Matchers.define :have_success_message do |message|
+  match do |page|
+    expect(page).to have_selector('div.alert.alert-success', text: message)
   end
 end
